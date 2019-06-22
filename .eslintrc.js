88a69e92fcebc5ae4f19efc71c253955d27fcc55
Alex Werffeli <alex.werffeli@blockfactory.com>
2019-06-22 16:28:07 +0200

    update eslint to allowImportExportEverywhere

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
     "ecmaVersion": 6,
+    "sourceType": "module",
+    "allowImportExportEverywhere": true
   },
   rules: {
     'no-console': 'off',
